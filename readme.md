09a0288b0344d5527a9e6aa89c24b46ae8c61748
Shihan Rehman <shihanrehman@gmail.com>
2015-10-30 23:57:45 -0400

    third commit

--- readme.md
+++ readme.md
@@ -1,4 +1,21 @@
+TIMELINE OF EVENTS
+
 10/29/2015 
 
--Read content from wikipedia page
--Configure Nokugiri and intial routes/gems
+-Analyzed environoment 
+-Read content from sample angel.com html page and displayed html in terminal
+-Configure Nokugiri and install initial routes/gems
+
+10/30/2015
+
+-Commit final changes to github
+-Analayzed environoment to parse HTML content
+
+
+11/2/2015 -  11/6/2015 
+
+I will revert to the folliwing items:
+ 	-Collect HTML from the pagination with a for loop implementation
+ 	-Parse content
+ 	-Save content to HTML and CSV files
+ 	-Possibly collect information from each listing if above tasks are succssful 
--- wikipedia_scraper.rb
+++ wikipedia_scraper.rb
@@ -1,7 +1,7 @@
-require 'open-uri'
-require 'nokogiri'
+require 'open-uri' #Installed by default, This gem allows us to access differnt URL's from within the text editor
+require 'nokogiri' #Not Installed by default: from Terminal, gem install nokogiri, This gem helps us read and search the HTML content of a webpage
 
-url = "https://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
-page = Nokogiri::HTML(open(url))
+url = "https://angel.co/jobs" #Save the URL in a variable called url
+page = Nokogiri::HTML(open(url))  #Open url and scan through the content of the webpage with Nokogiri; the content of the HTML in stored in 'page' variable
 
-puts page
+puts page #Display html in Terminal
